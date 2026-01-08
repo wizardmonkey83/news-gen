de4fc405f51a77f9b47c77669f0637aefeba25f0
Benjamin <benjamin630@icloud.com>
2026-01-08 13:17:34 -0800

    file structure and preliminary steps being finished

---
+++ config.py
@@ -0,0 +1,15 @@
+# sets the project settings
+
+from decouple import config
+
+PROJECT_ID = config("GOOGLE_CLOUD_PROJECT_ID")
+LOCATION = config("GOOGLE_CLOUD_REGION")
+
+# for testing purposes its a bit cheaper than 3.0
+TEXT_MODEL = "gemini-2.5-pro"
+VIDEO_MODEL = "veo-3.1-fast-generate-preview"
+
+# for testing ttheres no need to generate videos or post to socials
+MOCK_VIDEO = True
+MOCK_SOCIAL = True
+
---
+++ core/state.py
@@ -0,0 +1,22 @@
+# weird python concept if you ask me. essentially defines what the agent can remember/be passed
+
+from typing import TypedDict, List, Optional
+
+class AgentState(TypedDict):
+    # the topic input for the news story
+    topic: str
+
+    # fields are optional to avoid errors arisng from empty fields
+    # ingestion
+    news_summary: Optional[str]
+    source_urls: List[str]
+
+    # creation
+    script: Optional[str]
+    video_prompt: Optional[str]
+    # link to the final video
+    video_url: Optional[str]
+
+    # status tracking
+    is_complete: bool
+    error: Optional[str]
