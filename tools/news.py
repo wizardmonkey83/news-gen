1321f34bc5db34f1d1b71dc7abe9ef66f85115d3
Benjamin <benjamin630@icloud.com>
2026-01-09 08:23:37 -0800

    think news collection is sound

--- tools/news.py
+++ tools/news.py
@@ -1,12 +1,13 @@
 from google import genai
 from google.genai import types
 from collections import defaultdict
-import datetime
+from datetime import date
 from config import TEXT_MODEL
 
+client = genai.Client()
+
 def collect_news(topic: str):
-    now = datetime.now()
-    client = genai.Client()
+    today = date.today()
 
     grounding_tool = types.Tool(
         google_search=types.GoogleSearch()
@@ -16,27 +17,46 @@ def collect_news(topic: str):
         tools=[grounding_tool]
     )
 
-    response = client.models.generate_content(
-        model=TEXT_MODEL,
-        # this will have to be dynamic
-        contents=f"Today is {now}. Search for the latest news about {topic}",
-        config=config,
-    )
+    try:
+        response = client.models.generate_content(
+            model=TEXT_MODEL,
+            # this will have to be dynamic
+            # also consider adding region to the search
+            contents=f"Today is {today}. Search for the latest news about {topic}",
+            config=config,
+        )
 
-    summary = response["candidates"]["content"]["parts"]["text"]
-    sources = defaultdict(list)
-    
-    # time complexity might be an issue
-    for url in response["candidates"]["groundingMetadata"]["groundingChunks"]:
-        for web in url:
-            title = web["title"]
-            uri = web["uri"]
-            sources[title].append(uri)
+        if response.candidates:
+            candidate = response.candidates[0]
+
+            if candidate.content and candidate.content.parts:
+                summary = response.candidates.content.parts[0].text
+            else:
+                summary = "No summary generated."
+
+            sources = defaultdict(list)
             
-    contents = {
-        "summary": summary,
-        "sources": sources,
-    }
+            if candidate.grounding_metadata and candidate.grounding_metadata.grounding_chunks:
+                for chunk in candidate.grounding_metadata.grounding_chunks:
+                    if chunk.web:
+                        title = chunk.web.title
+                        uri = chunk.web.uri
+                        sources[title].append(uri)
+            
+        else:
+            # this should be exceedingly rare fingers crossed
+            contents = {
+                "summary": "No news found.",
+                "sources": {}
+            }
+            return contents
+            
+        contents = {
+            "summary": summary,
+            "sources": dict(sources),
+        }
 
-    return contents
-
+        return contents
+    # in case of an API issue
+    except Exception as e:
+        return {"summary": "Error fetching news.", "sources": {}}
