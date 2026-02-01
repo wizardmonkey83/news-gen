d2e69e063c66642cda62fd2e546e0b0ab194a12a
Benjamin <benjamin630@icloud.com>
2026-02-01 15:03:51 -0800

    i think metric extraction is working. havent tested though....

--- tools/engagement.py
+++ tools/engagement.py
@@ -1,11 +1,45 @@
 from atproto import Client, AtUri
 from config import BSKY_USERNAME, BSKY_PASSWORD
+from datetime import datetime
+import time
+from atproto_client.models.app.bsky.feed.defs import ThreadViewPost, BlockedPost, NotFoundPost
+
+def extract_bsky_replies(thread: str):
+    # from atproto docs. could not be more confusing
+    if not isinstance(thread, ThreadViewPost):
+        return None
+
+    post = thread.post
+    scraped_at  = str(datetime.now())
+
+    post_metrics = {
+        "uri": post.uri,
+        "author_handle": post.author.handle,
+        "text": post.record.text,
+        "posted_at": post.indexed_at,
+        "scraped_at": scraped_at,
+        "metrics": {
+            "likes": post.like_count or 0,
+            "reposts": post.repost_count or 0,
+            "replies": post.reply_count or 0,
+        },
+        "replies": [],
+    }
+
+    if thread.replies:
+        for reply in thread.replies:
+            processed_reply = extract_bsky_replies(reply)
+            if processed_reply:
+                post_metrics["replies"].append(processed_reply)
+
+    return post_metrics
+            
 
 def review_bsky_metrics(bsky_post_urls: list):
     client = Client()
     client.login(BSKY_USERNAME, BSKY_PASSWORD)
 
-    metrics = {}
+    metrics = []
     for url in bsky_post_urls:
         try:
             parts = url.split("/")
@@ -21,17 +55,25 @@ def review_bsky_metrics(bsky_post_urls: list):
             at_uri = AtUri.from_str(f"at://{did}/app.bsky.feed.post/{rkey}")
         except Exception as e:
             print(f"Error parsing url: {e}")
-            return
-
+            continue
+        
         try:
-            data = client.get_post_thread(uri=at_uri)
+            data = client.get_post_thread(uri=at_uri, depth=1000, parent_height=0)
             thread = data.thread
 
-            if not hasattr(thread, "post"):
+            if not isinstance(thread, ThreadViewPost):
                 print(f"Unable to find post. URL: {url}")
-                return
-            
-            post = data.thread.post
-            if hasattr(thread, "replies") and thread.replies:
-                for reply in thread.replies:
-                    
+                continue
+
+            post_metrics = extract_bsky_replies(thread, url)
+
+            if post_metrics:
+                post_metrics["url"] = url
+                metrics.append(post_metrics)
+
+        except Exception as e:
+            print(f"Error occured: {e}")
+            continue
+
+        time.sleep(1)
+    return metrics
